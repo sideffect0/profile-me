f16aea88e010d4a916c14731d4ffccfb2bd964bd
sideffect0 <mail3renjith@gmail.com>
2020-05-13 19:30:19 +0530

    add service skelton

--- backend/src/main.go
+++ backend/src/main.go
@@ -6,7 +6,12 @@ func main() {
   app := fiber.New()
 
   app.Get("/:service", func(c *fiber.Ctx) {
-    println(c.Params("value"))
+    println(c.Params("service"))
+    c.Send("200 OK")
+  })
+
+  app.Post("/:service", func(c *fiber.Ctx) {
+    r := c.FormValue("resource")
     c.Send("200 OK")
   })
 
